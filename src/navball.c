cc9352edaa52b1e54f71c2cf0baf3f65c1509cae
infthi <infthi.inbox@gmail.com>
2015-02-01 22:57:05 +0300

    Painting navball circle

--- src/navball.c
+++ src/navball.c
@@ -1,19 +1,28 @@
 #define __NAVBALL__
 #include <navball.h>
 
-void canvas_update_proc(Layer *this_layer, GContext *ctx) {
+static void draw_interface(Layer *this_layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(this_layer);
 
-  // Get the center of the screen (non full-screen)
-  GPoint center = GPoint(bounds.size.w / 2, (bounds.size.h / 2));
 
-  // Draw the 'loop' of the 'P'
+  // Get the center of the navball
+  int radius = bounds.size.w / 2;
+  GPoint center = GPoint(radius, (bounds.size.h - radius));
   graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_circle(ctx, center, 40);
+  graphics_fill_circle(ctx, center, radius);
   graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_circle(ctx, center, 35);
+  graphics_fill_circle(ctx, center, radius-1);
 
   // Draw the 'stalk'
-  graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_rect(ctx, GRect(32, 40, 5, 100), 0, GCornerNone);
+//  graphics_context_set_fill_color(ctx, GColorBlack);
+//  graphics_fill_rect(ctx, GRect(32, 40, 5, 100), 0, GCornerNone);
+}
+
+static bool interface_required = true;
+
+void canvas_update_proc(Layer *this_layer, GContext *ctx) {
+    if (interface_required){
+	interface_required = false;
+	draw_interface(this_layer, ctx);
+    }
 }
