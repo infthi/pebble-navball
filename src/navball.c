edb104431ed1f71e8d6ea45a117e76a8d5a49032
infthi <infthi.inbox@gmail.com>
2015-02-03 23:04:19 +0300

    Navball circle, bars

--- src/navball.c
+++ src/navball.c
@@ -1,20 +1,21 @@
 #define __NAVBALL__
 #include <navball.h>
+#include <geometry.h>
 
 static const GBitmap *navball_bitmap = NULL;
-uint8_t* bitmap_data = NULL;
+uint8_t *bitmap_data = NULL;
 
 TextLayer *power_layer, *acc_layer, *acc_data_layer;
 
 void refresh(){
   int ri  = 0;
-  char b;
+//  char b;
 
   int i = 0;
   for (i=0; i<navball_bitmap->row_size_bytes*144;i++){
-    ri = rand();
-    b = ri;
-    bitmap_data[i] = b;
+//    ri = rand();
+//    b = ri;
+//    bitmap_data[i] = b;
   }
   layer_mark_dirty(bitmap_layer_get_layer(s_canvas_layer));
 }
@@ -36,32 +37,41 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
 }
 
 
-static void draw_interface(BitmapLayer *this_layer, GContext *ctx) {
-/*  GRect bounds = layer_get_bounds(this_layer);
-
-
-  // Get the center of the navball
-  int radius = bounds.size.w / 2 - 1;
-  GPoint center = GPoint(radius, (bounds.size.h-radius));
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_circle(ctx, center, radius);
-  graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_circle(ctx, center, radius-1);
-
-  // Draw the 'stalk'
-//  graphics_context_set_fill_color(ctx, GColorBlack);
-//  graphics_fill_rect(ctx, GRect(32, 40, 5, 100), 0, GCornerNone);
-*/
+void draw_part(int row_size, int i, int k) {
+  if ((k<0)||(k>=144)){
+    return;
+  }
+  int offset = row_size*(i)+(k/8);
+  uint8_t flag = 1<<(k%8);
+  bitmap_data[offset] |= flag;
 }
 
-static bool interface_required = true;
-
-/*void navball_update_proc(BitmapLayer *this_layer, GContext *ctx) {
-    if (interface_required){
-	interface_required = false;
-	draw_interface(this_layer, ctx);
+static void draw_interface() {
+  int i,k;
+  int row_size = navball_bitmap->row_size_bytes;
+  uint8_t flag;
+  int offset;
+  for (i=1; i<72; i++){
+    uint8_t begin = circle_144[i];
+    uint8_t end = circle_144[i-1];
+    if (begin==end)
+      end++;
+    for (k=begin; k<end; k++){
+//top left
+      draw_part(row_size, i, k);
+//top right
+      draw_part(row_size, i, 143-k);
+//bottom right
+      draw_part(row_size, 144-i, 143-k);
+//bottom left
+      draw_part(row_size, 144-i, k);
+//G bar
+      draw_part(row_size, 144-i, 153-k);
+//power bar
+      draw_part(row_size, 144-i, k-10);
     }
-}*/
+  }
+}
 
 TextLayer *text_layer_configure(GRect rect){
   TextLayer *layer = text_layer_create(rect);
@@ -79,6 +89,9 @@ void navball_init(BitmapLayer *this_layer) {
   GSize size = layer_get_bounds(bitmap_layer_get_layer(this_layer)).size;
   navball_bitmap = gbitmap_create_blank(size);
   bitmap_data = (uint8_t*)navball_bitmap->addr;
+  
+  draw_interface();
+
   bitmap_layer_set_bitmap(this_layer, navball_bitmap);
 
   power_layer = text_layer_configure(GRect(0, size.h-21, 84, 21));
@@ -86,7 +99,7 @@ void navball_init(BitmapLayer *this_layer) {
   acc_data_layer = text_layer_configure(GRect(0, size.h-118, size.w, 50));
 
   uint32_t num_samples = 1;
-  accel_data_service_subscribe(num_samples, data_handler);
+//  accel_data_service_subscribe(num_samples, data_handler);
   text_layer_set_text(power_layer, "50%");
   text_layer_set_text(acc_layer, "1G");
   text_layer_set_text_alignment(acc_layer, GTextAlignmentRight);
