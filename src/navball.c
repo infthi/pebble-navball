207ebc0b1e1a8eb16eb42868f5c68bd2fb558920
infthi <infthi.inbox@gmail.com>
2015-02-04 21:36:23 +0300

    RCS rendering&logic

--- src/navball.c
+++ src/navball.c
@@ -5,19 +5,60 @@
 static const GBitmap *navball_bitmap = NULL;
 uint8_t *bitmap_data = NULL;
 
-TextLayer *power_layer, *acc_layer, *acc_data_layer;
+TextLayer *power_layer, *acc_layer, *sas_layer, *rcs_layer;
+TextLayer *acc_data_layer;
+
+static uint32_t durations[] = {100, 100, 100, 100, 100};
+
+
+void draw_part(int row_size, int i, int k) {
+  if ((k<0)||(k>=144)){
+    return;
+  }
+  int offset = row_size*(i)+(k/8);
+  uint8_t flag = 1<<(k%8);
+  bitmap_data[offset] |= flag;
+}
+
 
 void refresh(){
-  int ri  = 0;
-//  char b;
+//  layer_mark_dirty(bitmap_layer_get_layer(s_canvas_layer));
+}
 
-  int i = 0;
-  for (i=0; i<navball_bitmap->row_size_bytes*144;i++){
-//    ri = rand();
-//    b = ri;
-//    bitmap_data[i] = b;
+void update_rcs(bool on){
+  int i,k;
+  uint8_t filler = on?0xff:0;
+  int row_size = navball_bitmap->row_size_bytes;
+  for (i=1; i<72; i++){
+    uint8_t offset = 0;
+    uint8_t remaining = circle_144[i];
+    while (remaining>0){
+      if (remaining>=8){
+	bitmap_data[row_size*(i)+offset] = filler;
+        remaining -= 8;
+        offset++;
+      } else {
+        if (on){
+          bitmap_data[row_size*(i)+offset] |= remaining_left[remaining];
+        } else {
+          bitmap_data[row_size*(i)+offset] &= ~remaining_left[remaining];
+        }
+        remaining = 0;
+      }
+    }
   }
+  text_layer_set_text_color(rcs_layer, on?GColorBlack:GColorWhite);
   layer_mark_dirty(bitmap_layer_get_layer(s_canvas_layer));
+  if (on){
+    vibes_double_pulse();
+  } else {
+    vibes_enqueue_custom_pattern((VibePattern){.durations=durations, .num_segments=5});
+  }
+}
+
+void init_rcs(){
+  update_rcs(bluetooth_connection_service_peek());
+  bluetooth_connection_service_subscribe(update_rcs);
 }
 
 
@@ -37,20 +78,10 @@ static void data_handler(AccelData *data, uint32_t num_samples) {
 }
 
 
-void draw_part(int row_size, int i, int k) {
-  if ((k<0)||(k>=144)){
-    return;
-  }
-  int offset = row_size*(i)+(k/8);
-  uint8_t flag = 1<<(k%8);
-  bitmap_data[offset] |= flag;
-}
 
 static void draw_interface() {
   int i,k;
   int row_size = navball_bitmap->row_size_bytes;
-  uint8_t flag;
-  int offset;
   for (i=1; i<72; i++){
     uint8_t begin = circle_144[i];
     uint8_t end = circle_144[i-1];
@@ -77,7 +108,7 @@ TextLayer *text_layer_configure(GRect rect){
   TextLayer *layer = text_layer_create(rect);
   text_layer_set_text_color(layer, GColorWhite);
   text_layer_set_background_color(layer, GColorClear);
-  text_layer_set_font(layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+  text_layer_set_font(layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   
   Layer *parent = bitmap_layer_get_layer(s_canvas_layer);
   layer_add_child(parent, text_layer_get_layer(layer));
@@ -96,11 +127,20 @@ void navball_init(BitmapLayer *this_layer) {
 
   power_layer = text_layer_configure(GRect(0, size.h-21, 84, 21));
   acc_layer = text_layer_configure(GRect(72, size.h-21, 72, 21));
+  rcs_layer = text_layer_configure(GRect(0, 0, 72, 21));
+  sas_layer = text_layer_configure(GRect(72, 0, 72, 21));
+
   acc_data_layer = text_layer_configure(GRect(0, size.h-118, size.w, 50));
+
+  text_layer_set_text(power_layer, "50%");
+  text_layer_set_text(acc_layer, "1G");
+  text_layer_set_text(rcs_layer, "RCS");
+  text_layer_set_text(sas_layer, "SAS");
+  text_layer_set_text_alignment(acc_layer, GTextAlignmentRight);
+  text_layer_set_text_alignment(sas_layer, GTextAlignmentRight);
+
+  init_rcs();
 
   uint32_t num_samples = 1;
 //  accel_data_service_subscribe(num_samples, data_handler);
-  text_layer_set_text(power_layer, "50%");
-  text_layer_set_text(acc_layer, "1G");
-  text_layer_set_text_alignment(acc_layer, GTextAlignmentRight);
 }
