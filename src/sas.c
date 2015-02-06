8463cc66717de179f1ede0cae10207a4bb296dc0
infthi <infthi.inbox@gmail.com>
2015-02-06 11:49:20 +0300

    SAS: fixed flickering

--- src/sas.c
+++ src/sas.c
@@ -2,7 +2,10 @@
 #include <navball.h>
 #include <sas.h>
 
-void update_sas(bool on){
+AppTimer *update_sas_timer = NULL;
+bool on = true;
+
+void update_sas_impl(){
   int i;
   uint8_t filler = on?0xff:0;
   int row_size = navball_bitmap->row_size_bytes;
@@ -30,6 +33,15 @@ void update_sas(bool on){
   }
   text_layer_set_text_color(sas_layer, on?GColorBlack:GColorWhite);
   refresh();
+  update_sas_timer = NULL;
+}
+
+void update_sas(bool enabled){
+  on = enabled;
+  if (update_sas_timer!=NULL){
+    app_timer_cancel(update_sas_timer);
+  }
+  update_sas_timer = app_timer_register(500, (AppTimerCallback) update_sas_impl, NULL);
 }
 
 void init_sas(){
