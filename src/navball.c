623b9dfa12dafbc18d7f94d674660ff52a51d1e8
infthi <infthi.inbox@gmail.com>
2015-02-08 16:54:29 +0300

    Made line rendering algorithm improvements more clear

--- src/navball.c
+++ src/navball.c
@@ -1,5 +1,7 @@
 #include <include/gui.h>
 
+#define REAL_BALL_SIZE 70
+
 uint8_t begin[2], end[2];
 uint8_t y_step;
 
@@ -21,51 +23,50 @@ inline void put_pixel(uint8_t x, uint8_t y){
 
 void draw_line_pure(int16_t ix0, int16_t iy0, int16_t ix1, int16_t iy1, bool on){
   int16_t x0,y0,x1,y1;
-  if (ix0<ix1){
-    x0 = ix0;
-    x1 = ix1;
-    y0 = iy0;
-    y1 = iy1;
-  } else {
-    x0 = ix1;
-    x1 = ix0;
-    y0 = iy1;
-    y1 = iy0;
-  }
-  int16_t dx=x1-x0;
-  int16_t dy=y1-y0;
+  int16_t dx=ix1-ix0;
+  int16_t dy=iy1-iy0;
 
   bool reverse = false;
   if (abs(dy)>abs(dx)){
+    //we must perform calculation along y axis; x and y are swapped for calculation and swapped back for rendering
     reverse = true;
-    int16_t tmp;
 
-    if (y0<y1) {
-      tmp = x0;
-      x0 = y0;
-      y0 = tmp;
+    if (iy0<iy1) {
+      x0 = iy0;
+      y0 = ix0;
 
-      tmp = x1;
-      x1 = y1;
-      y1 = tmp;
-      
-      tmp = dx;
+      x1 = iy1;
+      y1 = ix1;
+
+      int16_t tmp = dx;
       dx = dy;
       dy = tmp;
     } else {
-      tmp = x0;
-      x0 = y1;
-      y1 = tmp;
-    
-      tmp = x1;
-      x1 = y0;
-      y0 = tmp;
+      x0 = iy1;
+      y0 = ix1;
 
-      tmp = dx;
+      x1 = iy0;
+      y1 = ix0;
+
+      int16_t tmp = dx;
       dx = -dy;
       dy = -tmp;
     }
+  } else {
+    if (ix0<ix1){
+      x0 = ix0;
+      y0 = iy0;
+      x1 = ix1;
+      y1 = iy1;
+    } else {
+      x0 = ix1;
+      y0 = iy1;
+      x1 = ix0;
+      y1 = iy0;
 
+      dx = -dx;
+      dy = -dy;
+    }
   }
 
   int16_t diff_y = (dy>0)?1:-1;
@@ -91,6 +92,7 @@ void draw_line_pure(int16_t ix0, int16_t iy0, int16_t ix1, int16_t iy1, bool on)
       reverse?hide_pixel(y,x):hide_pixel(x,y);
     }
   }
+  y = y1;
 }
 
 inline void draw_line_relative(int16_t ix1, int16_t iy1, int16_t ix2, int16_t iy2, bool on){
@@ -135,8 +137,8 @@ void draw_level(){
 
 void render_navball(int16_t x, int16_t y, int16_t z, float inv_sqrt){
 //  float nxf = x*71.0f*inv_sqrt;
-  int16_t nx = -x*71*inv_sqrt;
-  int16_t ny = y*71*inv_sqrt;
+  int16_t nx = -x*REAL_BALL_SIZE*inv_sqrt;
+  int16_t ny = y*REAL_BALL_SIZE*inv_sqrt;
 
 //  int crt = inv_sqrt;
 //  int rev = 1.0f/inv_sqrt;
